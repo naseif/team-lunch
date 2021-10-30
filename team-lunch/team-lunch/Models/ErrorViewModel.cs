3e9cddeb790e40fecc8c96fc82d4fb60516d9d6b
naseif <d2mds21@gmail.com>
2021-10-30 18:20:16 +0200

    initial commit

---
+++ team-lunch/team-lunch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace team_lunch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
